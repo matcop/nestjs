f2adc78804bdcd5f20dfd2bf9771bfb6272c50b9
matcop <matcop@gmail.com>
2023-08-17 19:22:02 -0400

    objetos e interfaces

---
+++ 01-typescrip-intro/src/bases/01-types.ts
@@ -0,0 +1,6 @@
+export let name='marcelo';
+export const age:number =35;
+export const isValid:boolean=true;
+
+
+console.log({isValid});
---
+++ 01-typescrip-intro/src/bases/02-objects.ts
@@ -0,0 +1,36 @@
+export const pokemonIds=[1,2,30,40,50,66];
+
+pokemonIds.push(+'20');
+
+
+interface Pokemon {
+    id:number;
+    name:string;
+    // age?:number;
+    //age:number|undefined;
+    age:number
+
+}
+
+
+
+export const bulbsaur:Pokemon ={
+    id:54321,
+    name:'bulbasur',
+    age:5
+    
+}
+
+export const picachu:Pokemon ={
+    id:21,
+    name:'picachu',
+    age:1
+    
+}
+
+
+
+
+
+
+console.log(bulbsaur);
