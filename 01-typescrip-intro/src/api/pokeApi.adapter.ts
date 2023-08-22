e36446cdd8c07175be3630001faac9d3032e6cb8
matcop <matcop@gmail.com>
2023-08-22 14:11:55 -0400

    Resolver el principio de sustitucion

--- 01-typescrip-intro/src/api/pokeApi.adapter.ts
+++ 01-typescrip-intro/src/api/pokeApi.adapter.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
 //import { PokeapiResponse } from "../bases/interfaces/pokeapi-response.interface";
-
 //adaptar una funcionalidad externa de terceros a mi codigo
+export interface HttpAdapter{
+    get<T>(url:string):Promise<T>;
+}
+
+
+
 
 export class PokeApiFetchAdapter{
     async get<T>(url:string):Promise<T>{
         const resp = await fetch(url);
         const data:T= await resp.json();
-
+console.log('con Fetch');
         return data;
     }
 }
@@ -17,6 +22,7 @@ export class PokeApiAdapter{
 private readonly axios= axios;
     async get<T>(url:string):Promise<T>{
         const { data } = await this.axios.get<T>( url);
+        console.log('con axios');
         return data;
     }
 
--- 01-typescrip-intro/src/bases/04-injections.ts
+++ 01-typescrip-intro/src/bases/04-injections.ts
@@ -1,6 +1,6 @@
 //import axios from 'axios';
 import { Move, PokeapiResponse } from './interfaces/pokeapi-response.interface';
-import { PokeApiAdapter, PokeApiFetchAdapter } from '../api/pokeApi.adapter';
+import { HttpAdapter, PokeApiAdapter, PokeApiFetchAdapter } from '../api/pokeApi.adapter';
 
 
 export class Pokemon {
@@ -13,7 +13,7 @@ export class Pokemon {
         public readonly id: number, 
         public name: string,
         // Todo: inyectar dependencias
-        private readonly http: PokeApiAdapter
+        private readonly http: HttpAdapter
 
     ) {}
 
@@ -37,6 +37,6 @@ export class Pokemon {
 
 const pokeApiAxios =new PokeApiAdapter();
 const PokeApiFetch= new PokeApiFetchAdapter(); 
-export const charmander = new Pokemon( 4, 'Charmander',PokeApiFetch );
+export const charmander = new Pokemon( 4, 'Charmander',pokeApiAxios );
 
 charmander.getMoves();
